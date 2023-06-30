8cfd24e6c60af5e9d0b8709cb9e1f200837b19cc
MitchDuke <mdmsduke@googlemail.com>
2023-06-30 21:19:21 +0000

    Added division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractionQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -89,6 +91,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -140,4 +144,14 @@ function displayMultiplyQuestion(operand1, operand2) {
     
 }
 
+function displayDivisionQuestion(operand1, operand2) {
+
+    let divisionResult = operand1 * operand2;
+    operand1 = divisionResult;
+    operand2 = operand2;
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
+
+}
 
